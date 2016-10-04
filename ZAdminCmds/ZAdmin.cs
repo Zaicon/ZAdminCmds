62dd8269a53e912ab4743a89c2f0cbf138e22882
Zaicon Kiroshu <kzaicon@yahoo.com>
2016-10-04 08:20:04 -0500

    API 1.25 Update

--- ZAdminCmds/ZAdmin.cs
+++ ZAdminCmds/ZAdmin.cs
@@ -12,6 +12,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Timers;
 using Terraria;
 using TerrariaApi.Server;
@@ -31,13 +32,13 @@ namespace ZAdminCmds
 		public static double MIDNIGHT = 16200;
 	}
 
-	[ApiVersion(1, 24)]
+	[ApiVersion(1, 25)]
     public class ZAdmin : TerrariaPlugin
     {
 		public override string Name { get { return "ZAdminCmds"; } }
 		public override string Author { get { return "Zaicon"; } }
 		public override string Description { get { return "Misc Commands"; } }
-		public override Version Version { get { return new Version(1, 2, 7, 0); } }
+		public override Version Version { get { return Assembly.GetExecutingAssembly().GetName().Version; } }
 
 		public static Config config = new Config();
 		public static string configpath = "tshock/ZAdmin.json";
